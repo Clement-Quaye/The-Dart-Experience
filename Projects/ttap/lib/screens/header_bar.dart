4f5c7faa0ec2b7ddb31aac1cc8bb36ce46f7f23c
Clement A. Quaye <clemaddo@gmail.com>
2023-02-10 00:53:54 +0000

    Add Header Bar for 'My Ticket's' page

---
+++ Projects/ttap/lib/screens/header_bar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HeaderBar extends StatefulWidget {
+  const HeaderBar({Key? key}) :super(key: key);
+
+  @override
+  State<HeaderBar> createState() => _HeaderBar();
+}
+
+class _HeaderBarState extends State<HeaderBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      title: Text("My tickets"),
+      ),
+    );
+  }
+}
