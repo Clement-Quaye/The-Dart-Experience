f513981beabcf9fbc760e2b2b3589b17fa9d6714
Clement A. Quaye <clemaddo@gmail.com>
2023-02-21 23:31:05 +0000

    Start creating Task List view

---
+++ Projects/mytodo/lib/views/task_list_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../view_models/app_view_model.dart';
+
+class TaskListView extends StatelessWidget {
+  const TaskListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AppViewModel>(builder:(context, viewModel, child) {
+      return Container();
+    });
+  }
+}
